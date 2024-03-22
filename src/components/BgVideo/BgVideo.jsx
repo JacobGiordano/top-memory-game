2c54c15a040b28cc907d7ce399a7ab3858d53fdc
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-03-22 04:35:41 -0700

    Remove casting icon & unused div from bg video

--- src/components/BgVideo/BgVideo.jsx
+++ src/components/BgVideo/BgVideo.jsx
@@ -4,9 +4,14 @@ import video from "../../assets/videos/nebula-video.mp4";
 function BgVideo() {
   return (
     <div className='bg-video-container'>
-      <div className='overlay'>
-        <video src={video} className='bg-video' autoPlay loop muted></video>
-      </div>
+      <video
+        src={video}
+        className='bg-video'
+        autoPlay
+        loop
+        muted
+        disableRemotePlayback
+      ></video>
     </div>
   );
 }
