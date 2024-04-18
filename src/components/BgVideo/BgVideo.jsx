5da6f37dfd128db012f0011544935156a7e5b558
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-04-17 22:56:29 -0700

    Update approach to bg video to make it responsive & work on mobile

--- src/components/BgVideo/BgVideo.jsx
+++ src/components/BgVideo/BgVideo.jsx
@@ -1,17 +1,23 @@
 import "./BgVideo.css";
-import video from "/videos/nebula-video.mp4";
+import smallVideo from "/videos/nebula_25047_640×360.mp4";
+import mediumVideo from "/videos/nebula_25047_960×540.mp4";
+import largeVideo from "/videos/nebula_25047_1280×720.mp4";
+import extraLargeVideo from "/videos/nebula_25047_1920×1080.mp4";
+import { useEffect } from "react";
 
 function BgVideo() {
+  useEffect(() => {
+    const videoEl = document.querySelector(".bg-video");
+    videoEl ? (document.querySelector(".bg-video").defaultMuted = true) : null;
+  }, []);
   return (
     <div className='bg-video-container'>
-      <video
-        src={video}
-        className='bg-video'
-        autoPlay
-        loop
-        muted
-        disableRemotePlayback
-      ></video>
+      <video className='bg-video' autoPlay loop muted disableRemotePlayback>
+        <source src={smallVideo} media='(max-width: 639px)'></source>
+        <source src={mediumVideo} media='(max-width: 959px)'></source>
+        <source src={largeVideo} media='(max-width: 1279px)'></source>
+        <source src={extraLargeVideo}></source>
+      </video>
     </div>
   );
 }
